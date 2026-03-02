7a9b3b9ec811bd4d4e0b3ff43c143124f6c2106a
ruediuqqq <fantana.radu@yahoo.com>
2026-03-02 18:28:09 +0200

    Am adaugat ultimele modificari la cod

--- resource/run_sim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-colcon build --packages-select turtlebot_penalty
-source /home/simina/ros2_ws/install/setup.bash
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:/home/simina/ros2_ws/src/turtlebot_penalty/models
-ros2 launch turtlebot_penalty penalty_launch.py
